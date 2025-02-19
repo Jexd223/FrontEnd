9ce883f848e07c1aac631d44bb1540e5acb4072f
Jexd223 <s6606021611083@email.com>
2025-02-19 22:01:28 +0700

    Update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const path = require("path");
 
 const base_url = "http://localhost:5000";
 
-app.set("views",path.join(__dirname,"public/views"));
+app.set("views",path.join(__dirname,"/public/views"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
